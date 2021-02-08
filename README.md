e13f58eb64896c917d3dd4a4355f394488a1eb46
annadomenech <anna.domenech@aridhia.com>
2021-02-08 10:50:54 +0000

    fixed video and help tab

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # WORLD DEVELOPMENT INDICATORS
 
-**World Development Indicators** is the World Bank's premier compilation of cross-country comparable data on development. The World Development Indicators is a compilation of relevant, high-quality, and internationally comparable statistics about global development and the fight against poverty. 
+**World Development Indicators** is the World Bank's premier compilation of cross-country comparable data on development. They are a compilation of relevant, high-quality, and internationally comparable statistics about global development and the fight against poverty. 
 
 The database contains 1,400 time series indicators for 217 economies and more than 60 country groups, with data for many indicators going back more than 50 years. The main topics covered in the data are:
 
 - **Poverty and inequality** (poverty, prosperity, consumption, income distribution...)
 - **People** (people dynamics, education, labor, health, gender...)
 - **Environment** (agriculture, climate change, energy, biodiversity, water, sanitation...)
-- **Economy** (growth, economic structure, income and savings, trade, labor productivity...)
+- **Economy** (growth, economic structure, income and savings, trade, labour productivity...)
 - **States and Markets** (business, stock markets, military, communications, transport, technology)
 - **Global Links** (debt, trade, aid dependency, refugee, tourism, migration)
 
-Tha database for the world development indicators can be found here: https://datatopics.worldbank.org/world-development-indicators/ 
+The database for the world development indicators can be found here: https://datatopics.worldbank.org/world-development-indicators/ 
+
+
 ### Checkout and run
 
 You can clone this repository by using the command:
--- code/help_tab.R
+++ code/help_tab.R
@@ -1,15 +1,17 @@
-documentation_tab <- function() {
+documentation_tab <- function() {#
+  # Help tab
   tabPanel("Help",
+           # Header
            dashboardPage(
                      dashboardHeader(title = paste0("World Development Indicators RShiny mini-app"),
                                      titleWidth = 450),
-                     
+                    # Side bar to match app tab  
                      dashboardSidebar(
-                       p("This is the Help Tab, it will provide enough information to use the app",
+                       p("This is the Help Tab, it will provide information on how to use the app",
                          class = "form-group shiny-input-container"),
                        width = 300
                      ),
-                     
+                     # Style to match App style
                      dashboardBody(
                        tags$head(tags$style(HTML("
                        .skin-yellow .main-header .logo {
@@ -18,20 +20,30 @@ documentation_tab <- function() {
                        .skin-yellow .main-header .logo:hover {
                        background-color: #f39c12;
                        }"))),
-                       
-                       p("This RShiny mini-app allows you to visualize the changes from the 1960 to 2012 in life expectency of countries with different incomes."),
-                       p("The graph will be displayed on the main panel on the 'App' tab, be aware that it can take a moment to load. Once you see the graph you can:"),
-                       tags$ol(
-                         tags$li("Hover over the data points, this will display more information, such as country, population, year, life expectancy, region and income group."),
-                         tags$li("Move the slide input under the graph to display a different year"),
-                         tags$li("Click on 'Play' to start the authomatic motion of the slider input, this will animate the graph and you will see the changes of life-expectancy year by year."),
-                         tags$li("Click on a region of the legend to omit it from the graph, if you double-click you will only see that region displayed")
+                       fluidRow(column(
+                         6,
+                         h3("How to use the App"),
+                         p("This RShiny mini-app allows you to visualize the changes from the 1960 to 2012 in life expectency of countries with different incomes using the World Development
+                         indicators, which is a World's Bank premier compilation of cross-coutry data on development. This data is a compilation of relevant, high-quality and internationally
+                         comparable statistics about global development in the fight against poverty."),
+                         p("The graph will be displayed on the main panel on the 'App' tab, be aware that it can take a moment to load. Once you see the graph you can:"),
+                         tags$ol(
+                           tags$li("Hover over the data points, this will display more information, such as country, population, year, life expectancy, region and income group."),
+                           tags$li("Move the slide input under the graph to display a different year"),
+                           tags$li("Click on 'Play' to start the authomatic motion of the slider input, this will animate the graph and you will see the changes of life-expectancy year by year."),
+                           tags$li("Click on a region of the legend to omit it from the graph, if you double-click you will only see that region displayed")
+                         )
                        ),
-                       h3("Walkthrough video"),
-                       tags$video(src="world_development.mp4", type = "video/mp4", width="100%", height = "350", frameborder = "0", controls = NA),
-                       p(class = "nb", "NB: This mini-app is for provided for demonstration purposes, is unsupported and is utilised at user's 
-                       risk. If you plan to use this mini-app to inform your study, please review the code and ensure you are 
-                       comfortable with the calculations made before proceeding. ")
+                       column(
+                         6,
+                         h3("Walkthrough video"),
+                         tags$video(src="world_development.mp4", type = "video/mp4", width="100%", height = "350", frameborder = "0", controls = NA),
+                         p(class = "nb", "NB: This mini-app is for provided for demonstration purposes, is unsupported and is utilised at user's 
+                       risk. If you plan to use this mini-app to inform your study, please review the code and ensure you are comfortable with the calculations made before proceeding. ")
+                       )
+                       
+                       )
+
                      ),
                      skin = "yellow"
                      ))
