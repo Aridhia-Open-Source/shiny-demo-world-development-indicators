129f99af6ff45c1432b63a2245ac5b4ea3c56fab
Bert Overduin <bert.overduin@aridhia.com>
2025-02-27 11:06:47 +0000

    Added check to dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -1,7 +1,15 @@
-install.packages("shiny")
-install.packages("shinydashboard")
-install.packages("dplyr")
-install.packages("tidyr")
-install.packages("stringr")
-install.packages("googleVis")
-install.packages("plotly")
+package_install <- function(x, ...) {
+  for (i in x) {
+    # Check if package is installed
+    if (!require(i, character.only = TRUE)){
+      # If the package could not be loaded then install it
+      install.packages(i)
+    }
+  }
+}
+
+# Source this script to install all the libraries needed for the app
+
+packages <- c("shiny", "shinydashboard", "dplyr", "tidyr", "stringr", "googleVis", "plotly")
+
+package_install(packages)
